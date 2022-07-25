da8e740267bead3fb1e959eead067ee0b2ae2d6e
thiendang201 <thiendang201@gmail.com>
2022-07-26 03:45:10 +0700

    m

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-db_host = 'localhost'
-db_name = 'db_ssstutter01'
-db_username = 'root'
-db_password = '111111'
+db_host = 'us-cdbr-east-06.cleardb.net'
+db_name = 'heroku_fe88257f5958164'
+db_username = 'bb213440390683'
+db_password = '268d6115'
